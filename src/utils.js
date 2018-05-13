f84fa986f6c42a8a26af775ae3a42e26064bc361
Clare Pickering <clarelpickering@hotmail.com>
2018-05-13 20:44:56 +0100

    fix getRandomCards function

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export function getRandomCards(number, cards) {
   const randomCardsIndex = [];
   for (let n = number; n > 0; n--) {
-    randomCardsIndex.push(Math.round(Math.random() * cards));
+    randomCardsIndex.push(Math.floor(Math.random() * cards));
   }
   return randomCardsIndex;
 }
